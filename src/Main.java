8fd8fa45f60d2f614276c7750622b83081cabdb6
bwielk <bwielk@gmail.com>
2018-05-28 23:39:56 +0200

    Closing db

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
             Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Blazej W\\IdeaProjects\\TestDB\\test.db");
-        }catch(SQLException e){
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
+            statement.close();
+            connection.close();
+        }catch(SQLException e) {
             System.out.println(e);
-            e.printStackTrace();
         }
     }
 }
