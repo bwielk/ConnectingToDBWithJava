973828755f183c3c1e6d34fd79bc5a75985c9282
bwielk <bwielk@gmail.com>
2018-05-28 23:53:51 +0200

    Added more actions with the db

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,18 @@ public class Main {
     public static void main(String[] args) {
         try{
             Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Blazej W\\IdeaProjects\\TestDB\\test.db");
+            connection.setAutoCommit(true);
             Statement statement = connection.createStatement();
-            statement.execute("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("DROP TABLE contacts");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                                    " VALUES ('Blaise', '09871223443', 'blaise@blaise.bl')");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                                    " VALUES ('John', '09321231123', 'john@john.jo')");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                                    " VALUES ('Janette', '0923111123', 'janette@jane.ja')");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                                    " VALUES ('Esther', '01121231123', 'esther@esther.est')");
             statement.close();
             connection.close();
         }catch(SQLException e) {
