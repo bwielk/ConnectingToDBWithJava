ae1fe738c56dd4761f29d905ee7e9af805efebd4
bwielk <bwielk@gmail.com>
2018-05-29 20:58:03 +0200

    Refactor of db methods with adding constants

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,56 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 
 public class Main {
 
+    public static final String DB_NAME = "test.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\\\Users\\\\Blazej W\\\\IdeaProjects\\\\TestDB\\\\" + DB_NAME;
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String NAME_COLUMNS_CONTACTS = "name";
+    public static final String PHONE_COLUMNS_CONTACTS = "phone";
+    public static final String EMAIL_COLUMNS_CONTACTS = "email";
+
     public static void main(String[] args) {
+
         try{
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Blazej W\\IdeaProjects\\TestDB\\test.db");
-            connection.setAutoCommit(true);
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
             Statement statement = connection.createStatement();
-            statement.execute("DROP TABLE contacts");
-            statement.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts (name, phone, email)" +
-                                    " VALUES ('Blaise', '09871223443', 'blaise@blaise.bl')");
-            statement.execute("INSERT INTO contacts (name, phone, email)" +
-                                    " VALUES ('John', '09321231123', 'john@john.jo')");
-            statement.execute("INSERT INTO contacts (name, phone, email)" +
-                                    " VALUES ('Janette', '0923111123', 'janette@jane.ja')");
-            statement.execute("INSERT INTO contacts (name, phone, email)" +
-                                    " VALUES ('Esther', '01121231123', 'esther@esther.est')");
+            statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts" + "(" +
+                    NAME_COLUMNS_CONTACTS + " TEXT, " +
+                    PHONE_COLUMNS_CONTACTS + " INTEGER, " +
+                    EMAIL_COLUMNS_CONTACTS + " TEXT)");
+            insertNewContact(statement,"Blaise", 987123, "blaise@blaise.bl");
+            insertNewContact(statement,"John", 932123, "john@john.jo");
+            insertNewContact(statement,"Janette", 3111123, "janette@jane.ja");
+////            statement.execute("INSERT INTO contacts (name, phone, email)" +
+////                                    " VALUES ('Esther', '01121231123', 'esther@esther.est')");
+//            statement.execute("UPDATE contacts SET email = 'est@est.est' WHERE name = 'Esther'");
+//            statement.execute("DELETE FROM contacts WHERE name = 'Janette'");
+            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+            while(results.next()){
+                System.out.println(results.getString("name") + " " + results.getInt("phone") +
+                " " + results.getString("email"));
+            }
+            results.close();
             statement.close();
             connection.close();
         }catch(SQLException e) {
             System.out.println(e);
+        }
+    }
+
+    public static void insertNewContact(Statement statement, String name, int phoneNum, String email){
+        try {
+            statement.execute("INSERT INTO " + TABLE_CONTACTS + "" + " (" +
+                    NAME_COLUMNS_CONTACTS + ", " +
+                    PHONE_COLUMNS_CONTACTS + ", " +
+                    EMAIL_COLUMNS_CONTACTS + ")" +
+                    " VALUES ('" + name + "', ' " + phoneNum + "', '" + email + "')");
+        }catch(SQLException e){
+            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
